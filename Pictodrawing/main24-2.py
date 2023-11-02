3f2320202989bbb6ce6160ae828f4047af325632
soung <129130414+rambody542@users.noreply.github.com>
2023-11-02 13:39:27 +0900

    사진이미지를 그림으로 조절하고 바꿔 저장

---
+++ Pictodrawing/main24-2.py
@@ -0,0 +1,35 @@
+import numpy as np
+import cv2
+
+ff = np.fromfile(r'Pictodrawing\nrfsample.jpg', np.uint8)
+img = cv2.imdecode(ff, cv2.IMREAD_UNCHANGED)
+img = cv2.resize(img, dsize=(900, 600), fx=1.0, fy=1.0, interpolation=cv2.INTER_LINEAR)
+
+def onChange(pos):
+  pass
+
+cv2.namedWindow("Trackbar Windows")
+cv2.createTrackbar("sigma_s", "Trackbar Windows", 0, 200, onChange)
+cv2.createTrackbar("sigma_r", "Trackbar Windows", 0, 100, onChange)
+
+cv2.setTrackbarPos("sigma_s", "Trackbar Windows", 100)
+cv2.setTrackbarPos("sigma_r", "Trackbar Windows", 10)
+
+while True:
+  
+  if cv2.waitKey(100) == ord('q'):
+    break
+  
+  sigma_s_value = cv2.getTrackbarPos("sigma_s", "Trackbar Windows")
+  sigma_r_value = cv2.getTrackbarPos("sigma_r", "Trackbar Windows") / 100.0
+  
+  print("sigma_s_value:", sigma_s_value)
+  print("sigma_r_value:", sigma_r_value)
+  
+  cartoon_image = cv2.stylization(img, sigma_s=sigma_s_value, sigma_r=sigma_r_value)
+  
+  cv2.imshow("Trackbar Windows", cartoon_image)
+  
+  cv2.imwrite(r'Pictodrawing\output_cartoon_image.jpg', cartoon_image)
+
+cv2.destroyAllWindows()
