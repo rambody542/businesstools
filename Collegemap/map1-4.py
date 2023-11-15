cef7a0b64fbe2a4790d9b27da42078bc2141ac4e
soung <129130414+rambody542@users.noreply.github.com>
2023-11-15 11:46:47 +0900

    지도엑셀과 API 정보를 융합해 위치에 마크하기

---
+++ Collegemap/map1-4.py
@@ -0,0 +1,9 @@
+import folium
+
+map = folium.Map(location=[37,127], zoom_start=7)
+
+marker = folium.Marker([37.341435483, 126.733026596], popup='한국공학대학교', icon = folium.Icon(color='blue'))
+
+marker.add_to(map)
+
+map.save(r'Collegemap\uni_map.html')
